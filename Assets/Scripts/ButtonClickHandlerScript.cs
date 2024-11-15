80a582deada07ed933fdcfd42532d1a3b6bcde12
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2024-11-15 14:47:18 -0500

    Buttons and click events added

---
+++ Assets/Scripts/ButtonClickHandlerScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickHandlerScript : MonoBehaviour
+{
+    public void OnButtonClickEntrance()
+    {
+        Debug.Log("Entrance button clicked");
+    }
+
+    public void OnButtonClickRedwoodGrove()
+    {
+        Debug.Log("Redwood grove button clicked");
+    }
+
+    public void OnButtonClickPortrait()
+    {
+        Debug.Log("Portrait button clicked");
+    }
+
+    public void OnButtonClickToolWall()
+    {
+        Debug.Log("Tool wall button clicked");
+    }
+
+    public void OnButtonClickGoldenBoy()
+    {
+        Debug.Log("Golden boy button clicked");
+    }
+
+    public void OnButtonClickComicSketches()
+    {
+        Debug.Log("Comic sketches button clicked");
+    }
+
+    public void OnButtonClickJawbonePaintings()
+    {
+        Debug.Log("Jawbone paintings button clicked");
+    }
+
+    public void OnButtonClickUntitledPictures()
+    {
+        Debug.Log("Untitled pictures button clicked");
+    }
+
+    public void OnButtonClickBackCorner()
+    {
+        Debug.Log("Back corner button clicked");
+    }
+
+    public void OnButtonClickTwelvePassOne()
+    {
+        Debug.Log("Twelve pass one button clicked");
+    }
+
+    public void OnButtonClickRockWall()
+    {
+        Debug.Log("Rock wall button clicked");
+    }
+
+    public void OnButtonClickBackHallway()
+    {
+        Debug.Log("Back hallway button clicked");
+    }
+}
